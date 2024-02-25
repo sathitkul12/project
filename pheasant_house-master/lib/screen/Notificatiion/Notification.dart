fe5ba88ce695366aae612fb778e2ed44076e57c3
sathitkul12 <sathitkuy123@gmail.com>
2024-02-25 17:14:20 +0700

    commit

--- pheasant_house-master/lib/screen/Notificatiion/Notification.dart
+++ pheasant_house-master/lib/screen/Notificatiion/Notification.dart
@@ -1,23 +1,50 @@
-// notification_screen.dart
-
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationScreen extends StatelessWidget {
-  const NotificationScreen();
+  NotificationScreen({super.key});
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Notification Screen'),
-        backgroundColor: Color(0xFF7EA48F), // Set your desired color here
+        backgroundColor: Color(0xFF7EA48F),
       ),
       body: Center(
-        child: Text(
-          'This is the Notification Screen',
-          style: TextStyle(fontSize: 18.0),
+        child: ElevatedButton(
+          onPressed: () {
+            showNotification();
+          },
+          child: Text('Show Notification'),
         ),
       ),
+    );
+  }
+
+  Future<void> showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'my_app_notifications', // your unique channel id
+      'My App Notifications', // your channel name
+      channelDescription:
+          'Receive notifications from My App', // your channel description
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Notification Title',
+      'This is the notification content',
+      platformChannelSpecifics,
+      payload: 'item x',
     );
   }
 }
